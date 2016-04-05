16a1af74342f408b3522d660f5137134c5f42b8b
Akihisa Higuchi <akihisa.nq@gmail.com>
2016-04-06 00:00:00 +0900

    8枚組みと穴の位置を追加

--- spec/route2html_spec.rb
+++ spec/route2html_spec.rb
@@ -15,8 +15,9 @@ describe "route2html" do
 		expect(system("ruby #{bin} #{test_file}")).to eq true
 		expect(File.exist?(test_result)).to eq true
 
+		system("wkhtmltopdf --disable-smart-shrinking -s A4 -O Landscape -L 0mm -R 0mm -T 4mm -B 0mm  #{test_result} #{test_result.gsub(/\.html$/, ".pdf")}")
 		# system("wkhtmltopdf --disable-smart-shrinking -s A5 -O Landscape -L 4mm -R 4mm -T 4mm -B 0mm  #{test_result} #{test_result.gsub(/\.html$/, ".pdf")}")
 		# system("wkhtmltopdf --disable-smart-shrinking -s A6 -L 4mm -R 4mm -T 4mm -B 0mm  #{test_result} #{test_result.gsub(/\.html$/, ".pdf")}")
-		system("wkhtmltopdf --disable-smart-shrinking -s A7 -L 4mm -R 4mm -T 4mm -B 0mm  #{test_result} #{test_result.gsub(/\.html$/, ".pdf")}")
+		# system("wkhtmltopdf --disable-smart-shrinking -s A7 -L 4mm -R 4mm -T 4mm -B 0mm  #{test_result} #{test_result.gsub(/\.html$/, ".pdf")}")
 	end
 end
